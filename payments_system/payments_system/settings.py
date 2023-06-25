031d2770c973d4feb8803fc565b3a05287609198
rupesh237 <rupesh237@icloud.com>
2023-06-25 11:01:25 +0545

    two app khalti & stripe and migrate added

--- payments_system/payments_system/settings.py
+++ payments_system/payments_system/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'khalti_payment',
+    'stripe_payment',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
